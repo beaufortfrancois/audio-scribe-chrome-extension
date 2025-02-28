36d59434865c8ed695b35c351367dbc33d171aac
François Beaufort <beaufort.francois@gmail.com>
2025-02-28 11:27:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# "Audio scribe" Chrome Extension
+
+A simple Chrome Extension to demonstrate the [Audio Prompt API](https://github.com/webmachinelearning/prompt-api/pull/71) in Chrome.
---
+++ src/bridge.js
@@ -0,0 +1,33 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+window.addEventListener("message", async ({ data }) => {
+  try {
+    const audioCtx = new AudioContext();
+    const audio = await fetch(data.objectUrl);
+    const arrayBuffer = await audio.arrayBuffer();
+    const content = await audioCtx.decodeAudioData(arrayBuffer);
+
+    const session = await window.ai.languageModel.create();
+    const stream = session.promptStreaming([
+      { type: "audio", content },
+      "transcribe this audio",
+    ]);
+    for await (const chunk of stream) {
+      chrome.runtime.sendMessage({ chunk });
+    }
+    chrome.runtime.sendMessage({ chunk: "$END" });
+
+  } catch (error) {}
+});
